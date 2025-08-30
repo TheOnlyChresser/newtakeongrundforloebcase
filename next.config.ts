e0cc48a5c4a77fd3fa3e923baaad69c927218e94
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-08-30 11:20:16 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-};
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: process.env.NODE_ENV === 'production' ? '/newtakeongrundforloebcase' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/newtakeongrundforloebcase/' : '',
+}
 
 export default nextConfig;
