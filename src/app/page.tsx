81b6b78675d1ed60ccf919501ae811d26d4da662
TheOnlyChresser <120821352+TheOnlyChresser@users.noreply.github.com>
2025-08-30 11:54:19 +0200

    image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Index () {
             <div className="mt-32"/>
             <aside className="aside">
                 <div className="asideleftimage">
-                    <Image width={10000} height={10000} className="object-cover h-full w-full" src="/public/stemning.jpeg" alt="rock koncert"/>
+                    <Image width={10000} height={10000} className="object-cover h-full w-full" src="./images/stemning.jpeg" alt="rock koncert"/>
                 </div>
                 <div>
                     <h2 className="asidetitle">Tæt på musikken</h2>
@@ -104,7 +104,7 @@ export default function Index () {
                     <p className="asidetext">Det lokale erhvervsliv, Søby Kommune samt forskellige organisationer og fonde støtter projektet økonomisk på flotteste vis. Sammen med billetindtægterne og barsalget er det foreningens økonomiske grundlag.</p>
                 </div>
                 <div className="asiderightimage">
-                    <Image width={10000} height={10000} className="object-cover h-full w-full" src="/public/stemning3.jpeg" alt="rock koncert"/>
+                    <Image width={10000} height={10000} className="object-cover h-full w-full" src="/images/stemning3.jpeg" alt="rock koncert"/>
                 </div>
             </aside>
         </div>
